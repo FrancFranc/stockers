8c8e2b8f6075943639badcbbb476209e21bc35a4
Adan Rabadan <evilb4k4@gmail.com>
2017-05-30 13:20:19 -0700

    added handlerbar template and img to the hoem page

---
+++ public/scripts/view/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//Event Listener for "Search" button on homepage
+$('#submit').on('click', function(){ window.location = 'public/stock.html'});
+
+
+
+
+
+
+
+// Handlebars for the data shown in table
+(function(){
+  const tableView = {};
+
+  tableView.showFav = () => {
+
+  }
+})
