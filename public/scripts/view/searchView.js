462b97d2baff27d67fa8cbbc6b50e797dc1e0275
Spencer Gietzen <Speng1516@comcast.net>
2017-05-31 11:05:14 -0700

    search bar will show companies related to your search as you go

--- public/scripts/view/searchView.js
+++ public/scripts/view/searchView.js
@@ -7,6 +7,14 @@ var app = app || {};
 
   searchView.index = () => {
     console.log(app.search.searchResults);
+    $('#searchResults').html('');
+    let x = $('#search').offset().left;
+    let y = $('#search').offset().top;
+    $('#searchResults').css('display', 'inline-block').css('left', x).css('top', y + $('#search').height());
+    app.search.searchResults.results.forEach(result => {
+      $('#searchResults').append(`${result.name} (${result.symbol})<br />`);
+    });
+
   };
 
   module.searchView = searchView;
